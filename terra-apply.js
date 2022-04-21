700a9e50eb09beaa4a407b9b6a46817ac4414ab1
Diego Miglino <diego.miglino@leapsight.com>
2022-04-20 23:32:54 -0300

    output.tf to get the instance detailed values

--- terra-apply.js
+++ terra-apply.js
@@ -15,12 +15,14 @@ const memory = args[2] != undefined ? args[2] : 64
 create();
 
   async function create() {
+    
     if(instance_name == undefined) {
       console.log('You must indicate a instance name');
       return;
     }
 
-    // PREPARE TF FILE
+
+    // PREPARE TF FILES
 
     const file_name = 'terraform-config-files/generic-instance.tf';
     var fs = require('fs')
@@ -35,18 +37,19 @@ create();
       });
     });
 
-    // ENCODE USER_DATA
-    
-    // docker service error: (Cannot connect to the Docker daemon)
-    let user_data_decoded = "#cloud-boothook\n#!/bin/bash\nwhoami > /tmp/root-output.txt\ndate >> /tmp/date.txt\ndocker run hello-world 2>/tmp/docker.log\ndate >> /tmp/date.txt"
-    
-    // crashes the instance, can't connect via ssh: (same with and without 'sudo')
-    // let user_data_decoded = "#cloud-boothook\n#!/bin/bash\nwhoami > root-output.txt\nsudo usermod -a -G docker $USER\nsudo systemctl enable docker.service\nsudo systemctl start docker.service\nsudo chmod 666 /var/run/docker.sock\ndocker run hello-world 2>docker.log"
-    
-    // crashes the instance, can't connect via ssh: (same with and without 'sudo')
-    // let user_data_decoded = "#cloud-boothook\n#!/bin/bash\newhoami > root-output.txt\n~/InfraDockerBot/configDocker.sh 2>docker.log\ndocker run hello-world 2>docker.log"
-    
-    let user_data_encoded = Buffer.from(user_data_decoded).toString("base64");
+    const output_file_name = 'terraform-config-files/generic-output.tf';
+    var fs = require('fs')
+    fs.readFile(output_file_name, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      var result = data.replace(/_REPLACE_/g, instance_name);
+
+      fs.writeFile('output.tf', result, 'utf8', function (err) {
+         if (err) return console.log(err);
+      });
+    });
+
 
     // TERRAFORM APPLY
 
---
+++ terraform-config-files/generic-output.tf
@@ -0,0 +1,4 @@
+output "Compute" {
+  description = "Compute created using NodeJS integration"
+  value       = oci_core_instance._REPLACE_
+}
